4bbac52bc4f38a4adcbb75c156ca3bf737dfaa71
valrus <imccowan@gmail.com>
2016-03-13 16:56:45 -0700

    Updates to work with custom tiny color picker.

--- mefi-comment-colors.user.js
+++ mefi-comment-colors.user.js
@@ -4,21 +4,21 @@
 // @description Color comments by username.
 // @include http://www.metafilter.com/*
 // @require https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js
-// @require https://cdnjs.cloudflare.com/ajax/libs/spectrum/1.8.0/spectrum.min.js
-// @resource customCSS https://cdnjs.cloudflare.com/ajax/libs/spectrum/1.8.0/spectrum.min.css
+// @require https://raw.githubusercontent.com/valrus/tinycolorpicker/master/lib/jquery.tinycolorpicker.js
 // @grant GM_addStyle
 // @grant GM_setValue
 // @grant GM_getValue
 // @grant GM_deleteValue
 // @grant GM_listValues
-// @grant GM_getResourceText
+// @grant GM_getResourceURL
 // @run-at document-start
 // ==/UserScript==
 
-var spectrumCSS = GM_getResourceText("customCSS");
-GM_addStyle(spectrumCSS);
-GM_addStyle('.sp-replacer { border: none; background-color: transparent; }');
-GM_addStyle('.sp-preview { width: 1em; height: 1em; }');
+GM_addStyle('.colorPicker { width: 20px; height: 20px; position: relative; clear: both; display: inline-block;}' );
+GM_addStyle('.colorPicker .color { width: 18px; height: 18px; padding: 1px; border: 1px solid #ccc; display: block; position: relative; z-index: 11; background-color: #EFEFEF; -webkit-border-radius: 27px; -moz-border-radius: 27px; border-radius: 27px; cursor: pointer; }');
+GM_addStyle('.colorPicker .colorInner { display: inline-block; width: 18px; height: 18px; -webkit-border-radius: 20px; -moz-border-radius: 20px; border-radius: 20px; }');
+GM_addStyle('#shared-dropdown { list-style: none; display: none; width: 18px; position: absolute; top: 28px; border: 1px solid #ccc; left: 0; z-index: 1000; }');
+GM_addStyle('#shared-dropdown li { height: 16px; width: 16px; cursor: pointer; }');
 
 // Tip from http://wiki.greasespot.net/@grant#Scope
 this.$ = this.jQuery = jQuery.noConflict(true);
@@ -27,6 +27,14 @@ var userColors = [];
 GM_listValues().forEach(function(val) {
     userColors[val] = GM_getValue(val);
 });
+
+/* Debugging
+var e = document.createElement("script");
+
+e.src = 'https://raw.githubusercontent.com/valrus/tinycolorpicker/master/lib/jquery.tinycolorpicker.js';
+e.type="text/javascript";
+document.getElementsByTagName("head")[0].appendChild(e);
+*/
 
 function hashCode(str) {
     var hash = 0;
@@ -42,19 +50,19 @@ function hashCode(str) {
 function addPicker($comment, $userlink, username, usercolor)
 {
     $userlink.after(
-        "<input " +
-            "type='text' " +
-            "class='colorpicker' " +
-            "value='" + (usercolor === undefined ? "#fff" : usercolor) + "' " +
+        "<div class='colorPicker' " +
+            "value='" + (usercolor === undefined ? "#ffffff" : usercolor) + "' " +
             "name='" + username +
-            "' />"
+
+            "'><a class='color'><div class='colorInner'></div></a><input type='hidden' class='colorInput'/></div>"
     );
 }
 
 function performColoring($comment, $userlink, username, usercolor)
 {
     $comment.css("color", usercolor === undefined ? "" : usercolor);
-    $comment.find('.colorpicker').spectrum("set", usercolor === undefined ? "#ffffff" : usercolor);
+    var $picker = $comment.find(".colorPicker");
+    $picker.data("plugin_tinycolorpicker").setColor(usercolor === undefined ? "#ffffff" : usercolor, $picker);
 }
 
 function forComments($comments, func)
@@ -67,17 +75,17 @@ function forComments($comments, func)
     });
 }
 
-function colorChange(color) {
-    var colorHex = color.toHexString().toLowerCase();
-    username = $(this).attr("name");
+function colorChange(elem, colorHex, colorRgb) {
+    var lcColor = colorHex.toLowerCase();
+    username = elem.attr("name");
     userHash = hashCode(username).toString();
-    if (colorHex == "#ffffff" || colorHex == "#fff") {
+    if (lcColor == "#ffffff" || lcColor == "#fff") {
         delete userColors[userHash];
         GM_deleteValue(userHash);
     }
     else {
-        userColors[userHash] = colorHex;
-        GM_setValue(userHash, colorHex);
+        userColors[userHash] = lcColor;
+        GM_setValue(userHash, lcColor);
     }
     var $comments = $("a[name]")
             .filter(function() { return this.name.match(/\d+/); })
@@ -88,13 +96,14 @@ function colorChange(color) {
 function initialSetup()
 {
     console.log(userColors);
+    $("div[role=main]").after(
+        '<div id="shared-track"></div><ul id="shared-dropdown"></ul>'
+    );
     var $comments = $("a[name]")
             .filter(function() { return this.name.match(/\d+/); })
             .next("div.comments");
     forComments($comments, addPicker);
-    $(".colorpicker").spectrum({
-        change: colorChange
-    });
+    $(".colorPicker").tinycolorpicker({sharedId: "shared", change: colorChange});
     forComments($comments, performColoring);
 }
 
